a2014a8d63973bdfb22fbb1d51013db4dbed173d
Vsevolod Hulchuk <vsevolod.hulchuk@gmail.com>
2022-10-21 01:09:02 +0200

    restructure and beautify kiss_icp node

--- kiss_icp/src/kiss_icp_node.py
+++ kiss_icp/src/kiss_icp_node.py
@@ -1,47 +1,40 @@
 #!/usr/bin/env python
-# license removed for brevity
-import rospy
-from std_msgs.msg import String
-from sensor_msgs.msg import PointCloud2, PointField
-import sensor_msgs.point_cloud2 as pc2
-import ros_numpy
-import os
-import datetime
-from functools import reduce
-import os
+
+# python libs
 from pathlib import Path
-import sys
 import time
-from typing import List
-from pathlib import Path
 import numpy as np
-from tqdm import trange
 from scipy.spatial.transform import Rotation
+
+# kiss_icp libs
+from kiss_icp.config import KISSConfig, load_config
+from kiss_icp.odometry import Odometry
+
+# ros libs
+import rospy
+import ros_numpy
+import rospkg
+
+# ros msgs libs
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import PointCloud2
 import tf
 
-from kiss_icp.config import KISSConfig, load_config, write_config
-from kiss_icp.metrics import absolute_trajectory_error, sequence_error
-from kiss_icp.odometry import Odometry
-from kiss_icp.visualizer import RegistrationVisualizer, StubVisualizer
-from geometry_msgs.msg import Pose, PoseStamped
-
-import rospkg
 rospack = rospkg.RosPack()
 pkg_path = rospack.get_path('kiss_icp')
 
-
+# Class to fit node to the existing kiss_icp.odometry.Odometry class
 class FakeData:
     def __init__(self, max_range=100):
         self.max_range = max_range
 
-class MyOdometryPipeline:
-    def __init__(
-        self, # dataset, # config: Path,
-        deskew: bool = False,
-        visualize: bool = False
-    ):
-        config = Path(str(pkg_path) + "/conf/default.yaml")
-        # self._dataset = dataset
+# Main class
+class KissIcpOdometry:
+    def __init__(self, deskew: bool = False, config: str = None):
+        if config is None:
+            config = Path(str(pkg_path) + "/conf/default.yaml")
+        else:
+            config = Path(config)
         self.config: KISSConfig = load_config(config)
         data = FakeData(100)
         self.config.data = data  # TODO(Nacho): Fix this config mess
@@ -49,14 +42,12 @@ class MyOdometryPipeline:
         self.odometry = Odometry(config=self.config, deskew=deskew)
         self.times = []
         self.poses = self.odometry.poses
-        self.pub = rospy.Publisher('estimated_pose', PoseStamped, queue_size=1)
-        self.cloud_pub = rospy.Publisher('velodyne_pcl', PointCloud2, queue_size=1)
-        self.br = tf.TransformBroadcaster()
 
-        # Visualizer
-        # self.visualizer = RegistrationVisualizer() if visualize else StubVisualizer()
-        # # if hasattr(self._dataset, "use_global_visualizer"):
-        # self.visualizer.global_view = True#self._dataset.use_global_visualizer
+        self.pub = rospy.Publisher('estimated_pose', PoseStamped, queue_size=1)
+        self.points_pub = rospy.Publisher( 'velodyne_pcl', PointCloud2, queue_size=1)
+        self.points_sub = rospy.Subscriber('/velodyne_points', PointCloud2, self.points_callback, queue_size=10)
+        
+        self.br = tf.TransformBroadcaster()
 
 
     def points_callback(self, msg: PointCloud2):
@@ -70,7 +61,7 @@ class MyOdometryPipeline:
         self.publish_pose(self.poses[-1])
         msg.header.frame_id = "base_link_estimate"
         msg.header.stamp = rospy.Time.now()
-        self.cloud_pub.publish(msg)
+        self.points_pub.publish(msg)
 
     def publish_pose(self, pose):
         R_array = pose[:3, :3]
@@ -94,21 +85,11 @@ class MyOdometryPipeline:
                         rospy.Time.now(),
                         "base_link_estimate",
                         "world")
-
-
-def talker():
-    rospy.init_node('subscribe', anonymous=True)
-    pipeline = MyOdometryPipeline(False, True)
-    sub = rospy.Subscriber('/velodyne_points', PointCloud2, pipeline.points_callback, queue_size=10)
-    rospy.spin()
-    
-def points_callback(msg: PointCloud2):
-    pc_array = ros_numpy.point_cloud2.pointcloud2_to_xyz_array(msg)
-    
-    rospy.loginfo(f"got msg {pc_array}")
  
 if __name__ == '__main__':
     try:
-        talker()     
+        rospy.init_node('kiss_icp', anonymous=True)
+        odometry = KissIcpOdometry(deskew=False)
+        rospy.spin()     
     except rospy.ROSInterruptException:
         pass
